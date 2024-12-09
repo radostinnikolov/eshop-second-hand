e5d6cec2fba5bb5f508729b4ce4cf1e4515ccb96
radostinnikolov <radostin.nikolov25@gmail.com>
2024-12-09 15:03:49 +0200

    .

--- myproject/settings.py
+++ myproject/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
-STATIC_ROOT = '/app/staticfiles/'
+STATIC_ROOT = "/tmp/app/staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/app/mediafiles/'
